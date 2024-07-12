3aaba190e321affd5bf649c9164747efbb5f071c
Sntoxss <contato.sntos@gmail.com>
2024-07-12 10:46:37 -0300

    "Tarefa 2 concluída"

--- open-music/src/scripts/index.js
+++ open-music/src/scripts/index.js
@@ -1,1 +1,12 @@
-/* Script Global */
+const genresButton = document.querySelectorAll(".genres__item");
+const removeButtonClass = document.querySelector('.genres__select')
+
+genresButton.forEach((button)=> {
+  button.addEventListener('click',(e) => {
+    e.preventDefault
+    genresButton.forEach((btn) => {
+      btn.classList.remove('genres__select')
+    });
+    button.classList.add('genres__select')
+  });
+});
